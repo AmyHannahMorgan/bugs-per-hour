3642fe5836a93ad3462fb83e172936f9bfd51982
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2021-01-21 20:19:27 +0000

    +send pass with request, check for auth fail

--- post.js
+++ post.js
@@ -3,13 +3,22 @@ const axios = require('axios').default;
 run();
 
 function run() {
-    axios.get('https://bugs-per-hour.amymorgan.vercel.app/api/apoidae')
+    axios.get(`https://bugs-per-hour.amymorgan.vercel.app/api/apoidae/?pass=${process.argv[2]}`)
     .then(res => {
         console.log(res)
     })
     .catch(err => {
-        console.log(err)
-        run();
+        if(err.reponse) {
+            if(err.response.status = 401) {
+                console.log(err);
+            }
+            else if(err.response.status = 502) {
+                console.log('attempt failed, retrying')
+                run();
+            }
+            else console.log(err);
+        }
+        else console.log(err)
     });
 }
 
