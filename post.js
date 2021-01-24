16d28852d163d1c28ff9f1d3ea42fc8412fb894f
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2021-01-24 20:11:22 +0000

    +add isopods endpoint to post script

--- post.js
+++ post.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 
 run();
 
-function run() {
+function apoidae() {
     axios.get(`https://bugs-per-hour.amymorgan.vercel.app/api/apoidae/?pass=${process.argv[2]}`)
     .then(res => {
         console.log(res)
@@ -14,11 +14,36 @@ function run() {
             }
             else if(err.response.status = 502) {
                 console.log('attempt failed, retrying')
-                run();
+                apoidae();
             }
             else console.log(err);
         }
         else console.log(err)
     });
+}
+
+function isopods() {
+    axios.get(`https://bugs-per-hour.amymorgan.vercel.app/api/isopods/?pass=${process.argv[2]}`)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        if(err.reponse) {
+            if(err.response.status = 401) {
+                console.log(err);
+            }
+            else if(err.response.status = 502) {
+                console.log('attempt failed, retrying')
+                isopods();
+            }
+            else console.log(err);
+        }
+        else console.log(err)
+    });
+}
+
+function run() {
+    apoidae();
+    isopods();
 }
 
